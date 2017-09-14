cca5f8cf1f3e3fd953bfcc2fdba4a2bf6e8039b5
Alan Vincent <alan.d.vincent@gmail.com>
2017-09-13 23:15:23 -0600

    home component placeholder structure and styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,18 +3,59 @@ import { Header } from 'semantic-ui-react';
 import About from './About';
 import Projects from './Projects';
 import Contact from './Contact';
+import profile from '../assets/profile.jpg'
+
+const styles ={
+  homeContainer: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    background: 'black', 
+    flexWrap: 'wrap',  
+  },
+  article: {
+    backgroundColor: 'rgba(63, 155, 191, .5)',
+    color: 'white',
+    flexBasis: '20%',
+    minWidth: '360px',
+    padding: '10px',
+  },
+  pictureDiv: {
+    minHeight: '56vh',
+    minWidth: '360px',
+    backgroundImage: `url(${profile})`,
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    flexBasis: '56%',
+    
+  },
+  profilePicture: {
+    maxWidth: '360px',
+    height: 'auto',
+
+
+  },
+}
 
 class Home extends Component {
   render() {
     return(
-      <div>
-      <Header as='h1' textAlign='center'>Home Component</Header>
-      <container>
-        welcome blurb
-      </container>
-      <About />
-      <Projects />
-      <Contact />
+      <div style={styles.homeContainer}> 
+        <div style={styles.article}>
+          <p>
+          Pellentesque habitant morbi tristique senectus et netus et 
+          malesuada fames ac turpis egestas. Morbi non ipsum quis nibh 
+          luctus rhoncus sed et lorem. Morbi malesuada diam at sem 
+          auctor semper at et diam. Quisque non risus quis mauris 
+          malesuada auctor at vestibulum purus. In egestas augue a nisl 
+          varius, id sagittis dui porta. Mauris finibus metus ut turpis 
+          consequat feugiat. Etiam magna lorem, tincidunt in consequat 
+          quis, blandit in lectus. Curabitur tempus nunc at metus 
+          facilisis, in lobortis velit posuere. Integer lacinia erat dui, 
+          faucibus egestas dui convallis a.
+          </p>
+        </div>
+        <div style={styles.pictureDiv}>
+        </div>
       </div>
     );
   }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,44 +4,100 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { handleLogout } from '../actions/auth';
 import { withRouter } from 'react-router-dom';
+import logo from '../assets/logo.gif';
+
+
+const styles ={
+  navdiv: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    backgroundColor: 'black',
+    minHeight: '70px',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  navImage: {
+    maxWidth: '100%',
+    height: 'auto',
+  },
+  navlist: {
+    listStyleType: 'none',
+    color: 'white',
+      li: {
+        border: "2px solid white",
+        color: 'white',
+        display: 'inline-block',
+        padding: '10px',
+        margin: '10px',
+          cta: {
+            backgroundColor: 'red',border: "2px solid white",
+            color: 'white',
+            display: 'inline-block',
+            padding: '10px',
+            margin: '10px',
+          }
+      }
+  },
+
+
+}
 
 class NavBar extends Component {
-  rightNavs = () => {
-    const { user, dispatch, history } = this.props;
+//   rightNavs = () => {
+//     const { user, dispatch, history } = this.props;
 
-    if(user.id) {
-      return(
-        <Menu.Menu position='right'>
-          <Menu.Item
-            name='Logout'
-            onClick={() => dispatch(handleLogout(history))}
-          />
-        </Menu.Menu>
-      );
-    } else {
-      return(
-        <Menu.Menu position='right'>
-          <Link to='/register'>
-            <Menu.Item name='Register' />
-          </Link>
-        </Menu.Menu>
-      );
-    }
-  }
+//     if(user.id) {
+//       return(
+//         <Menu.Menu position='right'>
+//           <Menu.Item
+//             name='Logout'
+//             onClick={() => dispatch(handleLogout(history))}
+//           />
+//         </Menu.Menu>
+//       );
+//     } else {
+//       return(
+//         <Menu.Menu position='right'>
+//           <Link to='/register'>
+//             <Menu.Item name='Register' />
+//           </Link>
+//         </Menu.Menu>
+//       );
+//     }
+//   }
 
-  render() {
-    return (
-      <div>
-        <Menu pointing secondary>
-          <Link to='/'>
-            <Menu.Item name='home' />
-          </Link>
-            <Menu.Item name='projects' />
-            <Menu.Item name='about' />
-            <Menu.Item name='contact me now!' />
-          { this.rightNavs() }
-        </Menu>
+//   render() {
+//     return (
+//       <div>
+//         <Menu pointing secondary>
+//           <Link to='/'>
+//             <Menu.Item name='home' />
+//           </Link>
+//             <Menu.Item name='projects' />
+//             <Menu.Item name='about' />
+//             <Menu.Item name='contact me now!' />
+//           { this.rightNavs() }
+//         </Menu>
+//       </div>
+//     )
+//   }
+
+  render () {
+    return(
+      <div style={styles.navdiv}>
+        <div>
+          <img src={logo} alt="Alan Vincent logo" style={styles.navImage}></img>
+        </div>
+        <div>
+          <ul style={styles.navlist}>
+            <li style={styles.navlist.li}>Home</li>
+            <li style={styles.navlist.li}>Projects</li>
+            <li style={styles.navlist.li}>About</li>
+            <li style={styles.navlist.li.cta}>Contact me now!</li>
+          </ul>
+        </div>
       </div>
+
     )
   }
 }
